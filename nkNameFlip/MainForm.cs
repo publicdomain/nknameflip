1a178530364f7c3a1a806e8cf9e7618283b8ba82
Victor <publicdomainvic@gmail.com>
2022-03-31 13:53:53 -0400

    Add OnFreeReleasesPublicDomainisToolStripMenuItemClick()

--- nkNameFlip/MainForm.cs
+++ nkNameFlip/MainForm.cs
@@ -292,7 +292,8 @@ namespace nkNameFlip
         /// <param name="e">Event arguments.</param>
         private void OnFreeReleasesPublicDomainisToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open our website
+            Process.Start("https://publicdomain.is");
         }
 
         /// <summary>
