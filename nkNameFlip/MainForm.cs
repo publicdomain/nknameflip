48bbab98d7bfd42d1e1086268c5402d67df9981c
Victor <publicdomainvic@gmail.com>
2022-03-31 14:06:07 -0400

    Add OnOriginalThreadDonationCodercomToolStripMenuItemClick()

--- nkNameFlip/MainForm.cs
+++ nkNameFlip/MainForm.cs
@@ -303,7 +303,8 @@ namespace nkNameFlip
         /// <param name="e">Event arguments.</param>
         private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open original thread
+            Process.Start("https://www.donationcoder.com/forum/index.php?topic=52177.0");
         }
 
         /// <summary>
