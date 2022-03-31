52b61247e8f5c998cd8fc410e8de26b56933ba44
Victor <publicdomainvic@gmail.com>
2022-03-31 14:08:47 -0400

    Add OnSourceCodeGithubcomToolStripMenuItemClick()

--- nkNameFlip/MainForm.cs
+++ nkNameFlip/MainForm.cs
@@ -314,7 +314,8 @@ namespace nkNameFlip
         /// <param name="e">Event arguments.</param>
         private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open GitHub repository
+            Process.Start("https://github.com/publicdomain/nknameflip");
         }
 
         /// <summary>
